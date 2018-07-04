be8b4831a59ef11000e1ebee137041606622332f
mtgto <hogerappa@gmail.com>
2018-07-05 01:27:28 +0900

    Add @types/atlassian-crowd-client

--- types/atlassian-crowd-client/api.d.ts
+++
@@ -1,8 +0,0 @@
-import { Settings } from "./settings";
-
-export default class CrowdApi {
-    settings: Settings;
-    constructor(settings: Settings);
-    request(method: string, path: string, data?: any): Promise<any>;
-    log(...args: any[]): void;
-}
--- types/atlassian-crowd-client/lib/models/attributes.d.ts
+++
@@ -1,7 +0,0 @@
-export default class Attributes {
-    readonly attributes: { [name: string]: any };
-
-    constructor(attributePairs: { [name: string]: any });
-    toCrowd(stringify?: (attr: any) => string): any;
-    static fromCrowd(attributesArr: ReadonlyArray<{ name: string }>, parse?: (json: string) => any): Attributes;
-}
