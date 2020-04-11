29a06b0af015a4706a4938b915eed19b32976b14
mtgto <hogerappa@gmail.com>
2020-04-11 22:59:04 +0900

    update webpack

--- webpack/prod.ts
+++ webpack/prod.ts
@@ -1,10 +1,6 @@
-import UglifyJsPlugin from "uglifyjs-webpack-plugin";
 import merge from "webpack-merge";
 import { config } from "./common";
 
 export default merge(config, {
     mode: "production",
-    optimization: {
-        minimizer: [new UglifyJsPlugin()],
-    },
 });
